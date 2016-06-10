0c79221485234b9d6c8abf9fca12530b6acde3f2
Chase Meadors <c.ed.mead@gmail.com>
2016-06-10 00:46:42 -0500

    Added a method to generate a manufactoria URL (in addition to the pre-existing method to parse one

--- program.js
+++ program.js
@@ -38,6 +38,8 @@ class Program {
         this.cells = [];
         this.changed = new signals.Signal();
 
+        this.metaInfo = null;
+
         for (let x = 0; x < cols; ++x) {
             this.cells.push([]);
             for (let y = 0; y < rows; ++y) {
@@ -77,9 +79,11 @@ class Program {
     }
 };
 
-function readLegacyProgramString(s) {
+function readLegacyProgramString(url) {
 
-    // [lvlString]&[codeString]&[metaInfo]
+    // pleasingfungus.com/Manufactoria/?[lvlString]&[codeString]&[metaInfo]
+
+    var s = url.split('?')[1];
 
     let i = 0;
 
@@ -106,6 +110,13 @@ function readLegacyProgramString(s) {
             attrs.rows = ctmParts[3];
             attrs.cols = ctmParts[3];
         }
+    }
+
+    if (attrs.lvl < 32) {
+        // The level string doesn't specify the program dimensions if it's one of the fixed levels, because it "knows" (ugh)
+        var fixedSizes = [5, 5, 7, 5, 9, 9, 9, 5, 7, 9, 7, 9, 13, 13, 11, 9, 9, 11, 13, 13, 13, 13, 13, 13, 7, 7, 9, 11, 11, 13, 13];
+        attrs.rows = fixedSizes[attrs.lvl - 1];
+        attrs.cols = fixedSizes[attrs.lvl - 1];
     }
 
     // Now parse the codeString part
@@ -165,13 +176,70 @@ function readLegacyProgramString(s) {
     p.setStart(Math.floor(p.cols / 2), 0);
     p.setEnd(Math.floor(p.cols / 2), p.rows - 1);
 
-    return p;
+    // Need to save this in order to generate a string from this program.
+    p.metaInfo = attrs;
 
+    return p;
 };
+
+function generateLegacyProgramString(prog) {
+    var lvlString = "lvl=32";
+    var metaInfo = "ctm=Program;(Generated);:*;" + prog.rows + ";3;0";
+
+    var codeParts = [];
+
+    var reverseTypeMap = new Map([
+        ['Conveyor', 'c'], ['WriteB', 'b'],   ['WriteR', 'r'],   ['WriteG', 'g'],
+        ['WriteY', 'y'],   ['BranchBR', 'p'], ['BranchGY', 'q'], ['CrossConveyor', 'i']
+    ]);
+
+    var x = 0, y = 0;
+    while (x < prog.rows) {
+        y = 0;
+        while (y < prog.rows) {
+            var cell = prog.getCell(x, y);
+            if (cell.type != 'Empty' && cell.type != 'Start' && cell.type != 'End') {
+                var type = reverseTypeMap.get(cell.type);
+                var column = x + Math.round(-0.5 * (prog.cols - 9) + 8);
+                var row = y + Math.round(-0.5 * (prog.cols - 9) + 3); // Lol
+                var orientation;
+                if (cell.type.startsWith('Branch')) {
+                    if (cell.orientation.equals(tmath.Mat2x2.kMROT3)) orientation = 0;
+                    if (cell.orientation.equals(tmath.Mat2x2.kMROT2)) orientation = 1;
+                    if (cell.orientation.equals(tmath.Mat2x2.kMROT1)) orientation = 2;
+                    if (cell.orientation.equals(tmath.Mat2x2.kMIR)) orientation = 3;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT3)) orientation = 4;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT2)) orientation = 5;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT1)) orientation = 6;
+                    if (cell.orientation.equals(tmath.Mat2x2.kID)) orientation = 7;
+                    // The remainder doesn't work with mirrored pieces yet because dihedral groups blegh.
+                } else if (!(cell.type == 'CrossConveyor')) {
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT3)) orientation = 0;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT2)) orientation = 1;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT1)) orientation = 2;
+                    if (cell.orientation.equals(tmath.Mat2x2.kID)) orientation = 3;
+                } else {
+                    if (cell.orientation.equals(tmath.Mat2x2.kID)) orientation = 5;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT3)) orientation = 1;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT2)) orientation = 0;
+                    if (cell.orientation.equals(tmath.Mat2x2.kROT1)) orientation = 3;
+                }
+                codeParts.push(type + column + ":" + row + "f" + orientation);
+            }
+            y += 1;
+        }
+        x += 1;
+    }
+
+    var codeString = "code=" + codeParts.join(';');
+    var final = [lvlString, codeString, metaInfo].join('&');
+    return "http://pleasingfungus.com/Manufactoria/?" + final;
+}
 
 export default {
     directions: dir,
     cellTypes,
     Program,
-    readLegacyProgramString
+    readLegacyProgramString,
+    generateLegacyProgramString
 };
